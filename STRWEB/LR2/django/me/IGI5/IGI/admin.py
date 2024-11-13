68d3a91d94159b643b6e4614fe08f88183bd262d
anterg0 <antonxll50@gmail.com>
2024-11-13 16:01:47 +0300

    STRWEB LR2 Done

---
+++ STRWEB/LR2/django/me/IGI5/IGI/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Product)
+# admin.site.register(Client)
+admin.site.register(Sales)
+admin.site.register(FAQModel)
+admin.site.register(Article)
+admin.site.register(PromoCode)
+admin.site.register(User)
+admin.site.register(Job)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(SalesItem)
---
+++ STRWEB/LR2/django/me/IGI5/IGI/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class IgiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'IGI'
